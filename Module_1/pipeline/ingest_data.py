24a20aba600b6ecd0abc12825c249cec0d3187ba
ADEP-123 <127460637+ADEP-123@users.noreply.github.com>
2026-03-09 21:44:49 +0000

    feat: :sparkles: docker compose

--- Module_1/pipeline/ingest_data.py
+++ Module_1/pipeline/ingest_data.py
@@ -2,9 +2,9 @@
 # coding: utf-8
 
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
-
 
 dtype = {
     "VendorID": "Int64",
@@ -30,24 +30,25 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_db = 'ny_taxi'
-    pg_port = 5432
-    
-    year = 2021
-    month = 1
-    
-    chunksize = 100000
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db} ')
+    engine = create_engine(
+        f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
+    )
 
-    
     df_iter = pd.read_csv(
         url,
         dtype=dtype,
@@ -55,22 +56,22 @@ def run():
         iterator=True,
         chunksize=chunksize,
     )
-    
-    target_table = 'yellow_taxi_data'
-    first = True;
+
+    first = True
     for df_chunk in tqdm(df_iter):
-        if (first):
+        if first:
             df_chunk.head(0).to_sql(
-                name = target_table,
-                con = engine,
-                if_exists = 'replace'
+                name=target_table,
+                con=engine,
+                if_exists='replace'
             )
             first = False
+
         df_chunk.to_sql(
-            name = target_table,
-            con = engine,
-            if_exists = 'append'
+            name=target_table,
+            con=engine,
+            if_exists='append'
         )
-        
+
 if __name__ == '__main__':
     run()
